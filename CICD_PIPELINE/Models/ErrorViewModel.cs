5fc8e2fffce59c466a9479a5286181020f97b12f
Shubham Subhash Jadhav <119497063+Shubham06012002@users.noreply.github.com>
2025-06-11 15:47:17 +0530

    Add project files.

---
+++ CICD_PIPELINE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CICD_PIPELINE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
